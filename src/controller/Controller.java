4b7d1c68195f12d03f3ababc0d6c489f6c9811b9
jaytohe <jaytohe@github.com>
2023-11-12 16:41:25 +0000

    moved mouse tracking func to controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,17 +1,34 @@
 package controller;
 
+import java.awt.Point;
+
 import model.Model;
+import model.ShapeType;
 
 
 public class Controller {
 
     private Model model;
+
+    private int startX, startY;
+
     public Controller(Model model) {
         this.model = model;
     }
 
 
-    public void incrementClicks() {
-        model.incrementClicks();
+    public void setShapeType(ShapeType shapeType) {
+        model.setShapeType(shapeType);
+    }
+
+
+    public void handleMousePressed(int x, int y) {
+
+        this.startX = x;
+        this.startY = y;
+    }
+
+    public void handleMouseReleased(int endX, int endY) {
+        model.drawShape(startX, startY, endX, endY);
     }
 }
