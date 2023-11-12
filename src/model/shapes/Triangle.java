6a89d0409a224650dab0be3fdebb8f128bc50b20
jaytohe <jaytohe@github.com>
2023-11-12 19:42:16 +0000

    Fixed triangle code; Now it is drawn based on the bounded rectangle.

--- src/model/shapes/Triangle.java
+++ src/model/shapes/Triangle.java
@@ -3,27 +3,24 @@ package model.shapes;
 import java.awt.Graphics;
 
 public class Triangle extends Shape {
-
-    private int width;
-    private int height;
-    private int minX;
-    private int minY;
     public Triangle(int startX, int startY, int endX, int endY) {
         super(startX, startY, endX, endY);
 
-        width = Math.abs(getEndX() - getStartX());
-        height = Math.abs(getEndY() - getStartY());
-        minX = Math.min(getStartX(), getEndX());
-        minY = Math.min(getStartY(), getEndY());
 
     }
 
     @Override
     public void draw(Graphics g) {
-        //Draw the three lines to make us a nice triangle
-        g.drawLine(minX, minY, (minX - minY / 2), (minY + height));
-        g.drawLine(minX, minY, (minX + width / 2), (minY + height));
-        g.drawLine((minX - width / 2), minY + height, (minX + width / 2), minY + height);
+
+        int width = Math.abs(getEndX() - getStartX());
+        int height = Math.abs(getEndY() - getStartY());
+        int minX = Math.min(getStartX(), getEndX());
+        int minY = Math.min(getStartY(), getEndY());
+        
+        int[] xPoints = {minX, minX + width, minX + width / 2};
+        int[] yPoints = {minY + height, minY + height, minY};
+
+        g.drawPolygon(xPoints, yPoints, 3);
     }
 
     @Override
