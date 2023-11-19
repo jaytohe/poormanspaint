f09c2599e839813fac6c7552593956ba1fb5bf76
jaytohe <jaytohe@github.com>
2023-11-19 01:33:11 +0000

    Added centroid for isosceles triangle.

--- src/model/shapes/Triangle.java
+++ src/model/shapes/Triangle.java
@@ -1,6 +1,7 @@
 package model.shapes;
 
-import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
 
 public class Triangle extends Shape {
     public Triangle(int startX, int startY, int endX, int endY) {
@@ -10,8 +11,9 @@ public class Triangle extends Shape {
     }
 
     @Override
-    public void draw(Graphics g) {
-
+    public void draw(Graphics2D g) {
+        super.draw(g);
+        
         int width = Math.abs(getEndX() - getStartX());
         int height = Math.abs(getEndY() - getStartY());
         int minX = Math.min(getStartX(), getEndX());
@@ -21,6 +23,10 @@ public class Triangle extends Shape {
         int[] yPoints = {minY + height, minY + height, minY};
 
         g.drawPolygon(xPoints, yPoints, 3);
+
+        if (beforeRotation != null) {
+            g.setTransform(beforeRotation);
+        }
     }
     @Override
     public boolean contains(int x, int y) {
@@ -37,6 +43,26 @@ public class Triangle extends Shape {
 
         // Check if the point lies within the triangle based on the barycentric coordinates
         return s > 0 && t > 0 && (s + t) < 2 * area * sign;
+    }
+
+    @Override
+    protected Point2D getCentroid() {
+        int width = Math.abs(getEndX() - getStartX());
+        int height = Math.abs(getEndY() - getStartY());
+        int minX = Math.min(getStartX(), getEndX());
+        int minY = Math.min(getStartY(), getEndY());
+        
+        // The centroid of a triangle is given by ((x1+x2+x3)/3, (y1+y2+y3)/3)
+        int[] xPoints = {minX, minX + width, minX + width / 2};
+        int[] yPoints = {minY + height, minY + height, minY};
+
+        return new Point2D.Double((xPoints[0] + xPoints[1] + xPoints[2]) / 3, (yPoints[0] + yPoints[1] + yPoints[2]) / 3);
+    }
+
+    @Override
+    protected void scale() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'scale'");
     }
 
 }
